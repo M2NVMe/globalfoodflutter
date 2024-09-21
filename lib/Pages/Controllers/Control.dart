a23b72d81818fd49a431d9fec5eaeaf50b490995
M2NV <m2sjmail@gmail.com>
2024-09-21 18:53:19 +0700

    [BBW] FIX/ADD FEATURE

---
+++ lib/Pages/Controllers/Control.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+class UtamaController extends GetxController{
+  //posisi terakhir
+  var selectedindex = 0.obs;
+
+  //pindah menu
+  void changeMenu(int index) {
+    selectedindex.value = index;
+
+  }
+}
