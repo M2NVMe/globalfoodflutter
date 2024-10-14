038e3d272703c1d1dcc51031d893dade98c83578
M2NV <m2sjmail@gmail.com>
2024-10-14 08:49:16 +0700

    [BBW] add/fix

--- lib/datas/DatasController.dart
+++ lib/datas/DatasController.dart
@@ -8,7 +8,7 @@ import 'package:globalfoodflutter/Reuses/foodlistnobutton.dart';
 import 'package:globalfoodflutter/datas/newLocalDatabasethingamajig.dart';
 
 class datascontroller extends GetxController {
-  var homeitems = <CustomListItem>[].obs;
+  var homeitems = <CustomListItemNobutton>[].obs;
   var menuitems = <CustomListItem>[].obs;
   var popularitem = <CustomListItem>[].obs;
   var orderitem = <Foodlisticonbutton>[].obs;
@@ -25,14 +25,11 @@ class datascontroller extends GetxController {
 
   void loadhomeitems() {
     homeitems.addAll([
-      CustomListItem(image: "lib/drawable/fotomakanan/cendol.jpg", title: "Cendol", description: "Rp. 5,000 -- Regular cup", buttonText: "Order", buttonColor: Colors.green, buttonTextColor: Colors.white, onButtonPressed: () {addToOrders("lib/drawable/fotomakanan/cendol.jpg", "Cendol", "Rp. 5,000");}),
-      CustomListItem(image: "lib/drawable/fotomakanan/bolu.png", title: "Bolu pandan", description: "Rp. 10,000 -- Regular size", buttonText: "Order", buttonColor: Colors.green, buttonTextColor: Colors.white, onButtonPressed: () {addToOrders("lib/drawable/fotomakanan/bolu.png", "Bolu pandan", "Rp. 10,000");}),
-      CustomListItem(image: "lib/drawable/fotomakanan/burger1.jpg", title: "Burger", description: "Rp. 15,000 -- Regular size", buttonText: "Order", buttonColor: Colors.green, buttonTextColor: Colors.white, onButtonPressed: () {addToOrders("lib/drawable/fotomakanan/burger1.jpg", "Burger", "Rp. 15,000");}),
-      //CustomListItemNobutton(image: "lib/drawable/fotomakanan/burger1.jpg", title: "Burger", description: "Sejenis sandwich yang di beri topping sayur sayuran dan daging sebagai lauknya, rasanya enak dan mudah untuk dikonsumsi"),
-      //CustomListItemNobutton(image: "lib/drawable/fotomakanan/pizza1.jpg", title: "Pizza", description: "Makanan ikonik dari Italia, bentuknya bulat dan tekstur yang khas"),
-      //CustomListItemNobutton(image: "lib/drawable/fotomakanan/icecream1.jpg", title: "Es Krim", description: "Makanan ini dingin lho!, sebuah makanan yang terbuat dari campuran susu yang didinginkan, cocok untuk di konsumsi saat cuaca panas"),
-      //CustomListItemNobutton(image: "lib/drawable/fotomakanan/spaget1.jpg", title: "Spaghetti", description: "Makanan yang menyerupai sebuah mie namun berasal dari Itali, biasanya di campur dengan campuran yang bernama bolognese"),
-      //CustomListItemNobutton(image: "lib/drawable/fotomakanan/belgiumfries.png", title: "French Fries", description: "Olahan kentang yang dimana kentang tersebut digoreng hingga kering agar menciptakan cita rasa yang crunchy dan di taburi dengan garam untuk menambah rasa"),
+      CustomListItemNobutton(image: "lib/drawable/fotomakanan/burger1.jpg", title: "Burger", description: "Sejenis sandwich yang di beri topping sayur sayuran dan daging sebagai lauknya, rasanya enak dan mudah untuk dikonsumsi"),
+      CustomListItemNobutton(image: "lib/drawable/fotomakanan/pizza1.jpg", title: "Pizza", description: "Makanan ikonik dari Italia, bentuknya bulat dan tekstur yang khas"),
+      CustomListItemNobutton(image: "lib/drawable/fotomakanan/icecream1.jpg", title: "Es Krim", description: "Makanan ini dingin lho!, sebuah makanan yang terbuat dari campuran susu yang didinginkan, cocok untuk di konsumsi saat cuaca panas"),
+      CustomListItemNobutton(image: "lib/drawable/fotomakanan/spaget1.jpg", title: "Spaghetti", description: "Makanan yang menyerupai sebuah mie namun berasal dari Itali, biasanya di campur dengan campuran yang bernama bolognese"),
+      CustomListItemNobutton(image: "lib/drawable/fotomakanan/belgiumfries.png", title: "French Fries", description: "Olahan kentang yang dimana kentang tersebut digoreng hingga kering agar menciptakan cita rasa yang crunchy dan di taburi dengan garam untuk menambah rasa"),
     ]);
   }
 
